8765aaa1067c73971c41aa2fadcf624f09ce54b8
youssef1987 <elkarroutiy@gmail.com>
2025-12-13 21:05:00 +0100

    creazione card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Typography from "./Components/Typography.jsx";
+import Card from "./Components/Card.jsx";
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <Typography>Typography : normale</Typography>
       <br />
       <Typography title={true}>Typography: title</Typography>
+      <Card title="title" image="./codeim.jpg">
+        card
+      </Card>
     </>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import "./Card.css";
+
+function Card({ children, image, title }) {
+  return (
+    <div className="card">
+      <img src={image} className="img" />
+      <div className="card-body">
+        <h5 className="title">{title}</h5>
+        <p className="text">{children}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
